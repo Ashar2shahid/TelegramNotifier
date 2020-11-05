31cde5caa8570cd0603a39022148260c21a28a49
Ashar Shahid <ashar2shahid@gmail.com>
2020-11-05 15:03:21 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Telegram Notifier
+
+This was created by me for the purpose of getting notified about Delhi Visa Slots but can be extended to other things as well.
+
+Please create the API_id and hashes from https://my.telegram.org/
+Create a bot and get the access token
+
+## Create .env file
+
+Create an .env file with the following configuration:
+
+```
+API_ID=<YOUR_API_ID>
+API_HASH='<YOUR_API_HASH>'
+PHONE='<YOUR_PHONE_NUMBER>'
+USERNAME='<YOUR_USERNAME>'
+BOT_TOKEN='<YOUR_BOT_TOKEN>'
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ getChats.py
@@ -0,0 +1,40 @@
+from telethon.sync import TelegramClient, events
+from dotenv import load_dotenv
+import re
+import os
+
+load_dotenv(override=True)
+
+api_id = os.getenv("API_ID") #Personal API
+api_hash = os.getenv("API_HASH") #Personal API
+phone = os.getenv("PHONE") #Phone Number used for Personal API
+username = os.getenv("USERNAME") #Personal Username
+bot_token = os.getenv("BOT_TOKEN") #Bot API token
+
+GROUP_ENTITY_ID = -1001196383496 #Delhi Visa slots ID
+
+with TelegramClient(username, api_id, api_hash).start(phone=phone) as client:
+    with TelegramClient('bot',api_id,api_hash).start(bot_token=bot_token) as bot:
+
+        @bot.on(events.NewMessage()) #On Subscribe to Bot / On MessageBot
+        async def handler(event):
+            sender = await event.get_sender()
+            print(sender.username)
+            print(sender.id)
+            await event.reply("Thank you for subscribing to this bot, You will only recieve notification to book slot from this bot if you are part of the Delhi Visa Slots group and post regular updates, which this bot will be monitoring.")
+            await event.reply("You can customize the notification sound by clicking on my name and then clicking on notifications and then Customize")
+            
+
+        @client.on(events.NewMessage(GROUP_ENTITY)) #On Message from Group
+        async def handler(event):
+            if re.search(r'\bGO\b',event.raw_text,re.IGNORECASE): # if text contains GO message
+                usernames = [user.id for user in await client.get_participants(GROUP_ENTITY_ID)] # GET ALL PARTICIPANTS OF GROUP
+                for users in usernames:
+                    try:
+                        await bot.send_message(users, 'CHECK SLOTS RIGHT NOW')
+                    except Exception as e:
+                        continue
+
+            
+
+        client.run_until_disconnected()
